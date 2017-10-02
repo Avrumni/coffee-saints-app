2ff53cc53d95b3c33b58b3d2ec8704d6ea684b02
MotoMe Dev <dev@motome.com.au>
2017-10-03 08:46:44 +1100

    WIP: Add checkbox to user list, some other UX improvements
    [#151540075]

--- src/app/saint/saint-chooser/saint-chooser.component.tsx
+++ src/app/saint/saint-chooser/saint-chooser.component.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {connect, DispatchProp} from 'react-redux';
 import {Link} from "react-router-dom";
-import {Saint} from "../saint";
 import {SaintStore} from "../saint.store";
 import {RouteComponentProps} from "react-router";
 import {BuyerActions} from "../../buyer/buyer.actions";
@@ -11,7 +10,8 @@ class SaintChooserComponent extends React.Component<SaintChooserComponentProps, 
     constructor(props: SaintChooserComponentProps) {
         super(props);
         this.state = {
-            selectedSaints: []
+            selectedSaints: [],
+            toggleAll: true
         };
 
         this.props.dispatch(SaintActions.find());
@@ -19,65 +19,106 @@ class SaintChooserComponent extends React.Component<SaintChooserComponentProps, 
 
     public whoBuys(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        this.props.dispatch(BuyerActions.find(this.state.selectedSaints))
+
+        let saintIds: number[] = this.state.selectedSaints
+            .filter((selectedSaint) => {
+                return selectedSaint.isSelected ? selectedSaint : false;
+
+            }).map((selectedSaint) => {
+                return selectedSaint.saint.id;
+            });
+
+        console.log('selected', saintIds);
+
+        this.props.dispatch(BuyerActions.find(saintIds))
             .then(() => this.props.history.push('/buy'));
     }
 
-    public toggleSaint(event: React.ChangeEvent<HTMLInputElement>) {
-        // This is seriously outrageous...
-        let selectedSaints = this.state.selectedSaints;
-        let value = +event.target.value;
-        let existingIndex = selectedSaints.indexOf(value);
+    public toggleSaint(index: number) {
+        this.state.selectedSaints[index].isSelected =
+            !this.state.selectedSaints[index].isSelected;
 
-        if (event.target.checked && existingIndex === -1) {
-            selectedSaints.push(value);
-        }
-        else if (!event.target.checked && existingIndex > -1) {
-            selectedSaints.splice(existingIndex, 1);
-        }
+        this.setState({
+            ...this.state
+        });
+    }
+
+    public toggleAllSaints() {
+        let toggleAll = !this.state.toggleAll;
+
+        this.state.selectedSaints.forEach((selectedSaint) => {
+            selectedSaint.isSelected = !toggleAll;
+        });
+
         this.setState({
             ...this.state,
-            selectedSaints
+            toggleAll
         });
     }
 
     public render() {
-        let saints = this.props.saints.saintsList.saints;
         let saintsLoading = this.props.saints.saintsList.loading;
 
+        this.props.saints.saintsList.saints.forEach((saint) => {
+            this.state.selectedSaints.push({
+                saint: saint,
+                isSelected: false
+            });
+        });
+
         return (<form className="saint-chooser" onSubmit={(e) => (this.whoBuys(e))}>
-            <h1>Who is getting a coffee today?</h1>
-            <Link className="add action" to="/add">Add a saint</Link>
-            {this.saintList(saints, saintsLoading)}
-            <button type="submit">Find out who buys</button>
+            <div>
+                <h1>Select saints to find out who buys?</h1>
+                <button type="submit" disabled={!(this.props.saints.saintsList.saints.length > 0)}>Find out who buys
+                </button>
+                <Link className="add action" to="/add">Add a saint</Link>
+            </div>
+            <div>
+                {this.saintList(saintsLoading)}
+            </div>
+            <div>
+            </div>
         </form>);
     }
 
-    private saintList (saints: Saint[], loading: boolean) {
+    private saintList(loading: boolean) {
         if (loading) {
             return (<div className="loading">Loading...</div>);
         }
         else {
-            return (<ul className="saint-list">{saints.map((saint, index) => {
-                return this.saintItem(saint, index);
-            })}</ul>);
+            return (<ul className="saint-list">
+                <li>
+                    <label className="selectAll">Select All</label>
+                    <input name="saintSelectAll" ref="saintSelectAll" type="checkbox"
+                           onChange={() => {
+                               this.toggleAllSaints();
+                           }}/>
+                </li>
+                {this.state.selectedSaints.map((selectedSaint, index) => {
+                    return this.saintItem(selectedSaint, index);
+                })}</ul>);
         }
     }
 
-    private saintItem (saint: Saint, index: number) {
+    private saintItem(selectedSaint: any, index: number) {
         return (
             <li key={index}>
-                <input name="saintSelect" ref="saintSelect" id={'saint' + saint.id} type="checkbox" value={saint.id} onChange={(e) => {this.toggleSaint(e)}}/>
-                <label htmlFor={'saint' + saint.id}>
+                <input name="saintSelect" ref="saintSelect" id={'saint' + selectedSaint.saint.id} type="checkbox"
+                       value={selectedSaint.saint.id}
+                       checked={selectedSaint.isSelected}
+                       onChange={() => {
+                           this.toggleSaint(index)
+                       }}/>
+                <label htmlFor={'saint' + selectedSaint.saint.id}>
                     <div className="name">
-                        {saint.name}
+                        {selectedSaint.saint.name}
                         <div className="bought">
                             <span>Bought:</span>
-                            {saint.coffeeBought}
+                            {selectedSaint.saint.coffeeBought}
                         </div>
                         <div className="consumed">
                             <span>Consumed:</span>
-                            {saint.coffeeConsumed}
+                            {selectedSaint.saint.coffeeConsumed}
                         </div>
                     </div>
                     <div className="thumb"><img className="default" src="/assets/beansus.svg"/></div>
@@ -94,5 +135,6 @@ interface SaintChooserComponentProps extends DispatchProp<any>, RouteComponentPr
 }
 
 interface SaintChooserComponentState {
-    selectedSaints: number[]
+    selectedSaints: any[],
+    toggleAll: boolean
 }
